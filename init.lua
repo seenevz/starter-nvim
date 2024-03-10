f8a6c1a565edd5f47096b5871821faf00da04fb7
Sergio Neves <sernevess@gmail.com>
2024-03-10 13:25:54 +0000

    Add option to repgrip no ignore files

--- init.lua
+++ init.lua
@@ -379,12 +379,13 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
+        defaults = {
+          intial_mode = 'normal',
+        },
+        pickers = {
+          find_files = { no_ignore = true },
+          live_grep = { additional_args = { '--no-ignore', '-F' } },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
