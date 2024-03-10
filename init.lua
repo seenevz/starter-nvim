50f9ef3ed51aba2346ab6b088fd9296d734f0f93
Sergio Neves <sernevess@gmail.com>
2024-03-10 13:24:10 +0000

    Set termgui colour to true

--- init.lua
+++ init.lua
@@ -101,6 +101,7 @@ vim.g.term = 'wezterm'
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+vim.opt.termguicolors = true
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, for help with jumping.
